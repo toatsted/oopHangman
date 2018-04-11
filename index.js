17dcbe618902e811a1c2c98dd529d04c5a998fb8
Sam <samueljacksonhicks@gmail.com>
2018-04-11 09:39:41 -0500

    added a previously guessed letters list

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ let item;
 const startingLives = 7;
 let lives;
 
+let guesses;
+
 function start(){
 	item = new Word(words[Math.floor(Math.random() * words.length)]);
 	lives = startingLives;	
+	guesses = [];
 	guess();
 }
 
@@ -32,6 +35,7 @@ function guess(){
 		console.log("\033c" + "Lives: " + lives);
 		console.log(`
 			${item.display().join(" ")}
+			: ${guesses.join("-")}
 		`)
 		inquirer.prompt([{
 				name: "char",
@@ -47,8 +51,12 @@ function guess(){
 				}
 		}])
 		.then(answer => {
-			if(!item.guess(answer.char.toLowerCase()))
+			console.log(guesses.indexOf(answer.char));
+			if(!item.guess(answer.char.toLowerCase()) &&
+				guesses.indexOf(answer.char) < 0){
+				guesses.push(answer.char);
 				lives--;
+			}
 			if(item.isDone()){
 				console.log("\033c" + "Lives: " + lives + `
 
@@ -63,7 +71,7 @@ function guess(){
 		.catch(err => console.log(err))
 	} else {
 		console.log('\033c' + "Lives: 0" + `
-			
+
 			${item.wordString.split("").join(" ")}
 			You Lost!`);
 		repeat();		
